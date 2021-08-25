087b24f067ab8dfd77594a0e04cf1beeb2d9d171
Vivek Verma <vermavivek951@gmail.com>
2021-08-25 12:40:52 +0530

    initial commit

---
+++ library.js
@@ -0,0 +1,57 @@
+let myLibrary = [];
+const submitBtn = document.querySelector('#submitForm');
+const displayBooks = document.querySelector('#displayBooks');
+
+
+function Book(title , author , pages , haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+
+    this.info = function() {
+        return `${title} by ${author} , ${pages} pages , ${haveRead}`;
+    }
+}
+
+submitBtn.addEventListener('click' , (e) => {
+    const bookName = document.getElementById('bookname').value;
+    const authorName = document.getElementById('authorname').value;
+    const pages = document.getElementById('numberofpages').value;
+    const haveRead = document.getElementById('havereadornot').checked;
+
+    addBookToLibrary(bookName,authorName,pages,haveRead);
+    
+});
+
+function addBookToLibrary(bookName,authorName,pages,haveRead) {
+    if(haveRead) {
+        haveRead = "Already Read";
+    } else {
+        haveRead = "NotRead";
+    }
+    const book = new Book(bookName,authorName,pages , haveRead);
+    
+    myLibrary.push(book);
+    
+}
+
+function displayBooksInLibrary() {
+    for(let i=0 ; i<myLibrary.length ; i++) {
+        const bookItem = document.createElement('div');
+        bookItem.classList.add('bookItem');
+        bookItem.textContent = `BookName: ${myLibrary[i].title}
+                                    AuthorName: ${myLibrary[i].author}
+                                    NumberOfPages: ${myLibrary[i].pages}
+                                    haveRead: ${myLibrary[i].haveRead}`;
+       
+        displayBooks.appendChild(bookItem);
+    }
+}
+
+const showBooksBtn = document.querySelector('#showBooksBtn');
+showBooksBtn.addEventListener('click' , (e) => {
+    displayBooksInLibrary();
+    console.log("abc");
+});
+
